11ce80008faa7a2daa760ee0b3d06695cc6c3f5d
ensva002 <144826940+ensva002@users.noreply.github.com>
2023-11-06 11:50:29 +0100

    progressbar til bildekarusell.

--- bildekarusell.js
+++ bildekarusell.js
@@ -1,14 +1,24 @@
 const karusell = document.getElementById("karusell")
+const karusellbilde = document.getElementById("karusellbilde")
 const bilder = karusell.getAttribute("data-antall")
 const album = karusell.getAttribute("data-album")
+const progress = document.getElementById("progress")
+const delay = 4000
 
-let bilde = 1
-let timer = setInterval(countDown,3500)
+let bilde = 0
+let timer = setInterval(countDown,delay)
 
+progress.style.transition = delay+"ms"
+countDown()
 function countDown(){
-    bilde += 1
-    if (bilde > bilder) bilde = 1
-    karusell.src = "bilder/"+album+""+bilde+".jpg"
+    progress.style.transition = "0ms linear"
+    progress.style.width = "0%"
+    setTimeout(function(){
+        bilde += 1
+        if (bilde > bilder) bilde = 1
+        karusellbilde.src = "bilder/"+album+""+bilde+".jpg"
+        progress.style.transition = delay-100+"ms linear"
+        progress.style.width = "100%"
+    },50)
+
 }
-
-
